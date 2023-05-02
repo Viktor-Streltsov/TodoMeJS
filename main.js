dd753758005386a8eca5bc77d91475ff535f452f
Viktor <88923141+Viktor-Streltsov@users.noreply.github.com>
2023-05-03 03:28:24 +0600

    feat: add filter

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ let all = document.querySelector('.all');
 let topical = document.querySelector('.topical');
 let ending = document.querySelector('.ending');
 let block = document.querySelector('.block-text');
-let count = 0;
+let arr = [];
+
+// Созздание тега li по клику на кнопку
 
 btn.addEventListener('click', function() {
     let li = document.createElement('li');
     if(input.value !== '') {
         li.innerHTML = `<span class="text-li">${input.value}</span> <button class="edit">Editing</button> <button class="clear">Clear</button>`;
-        count++;
         block.append(li);
         input.value = '';
     };
+
+// Создание инпута для редактирования тега li
+
     let inp = document.createElement('input');
     li.children[1].addEventListener('click', function() {
         inp.value = li.children[0].textContent;
@@ -21,12 +25,34 @@ btn.addEventListener('click', function() {
         li.children[0].append(inp);
         li.children[1].disabled = true;
     });
+
+// Добавление нового измененого li через созданные инпут
+
     inp.addEventListener('blur', function() {
         li.children[0].textContent = this.value;
         li.children[1].disabled = false;
     })
+
+// Навешивание класа зачеркивания на выполненый li
     li.children[2].addEventListener('click', function() {
         li.children[0].classList.toggle('active');
     })
+console.log(arr);
+// Фильтрация масива li
+return arr.push(li);
 });
+
+
+    topical.addEventListener('click', function() {
+        let clearLi = arr.filter(function(elem) {
+            if(elem.children[0].classList.contains('active')) {
+                return elem;
+            }
+            block.innerHTML = '';
+        });
+        for(let elem of clearLi) {
+            block.innerHTML += `<li>${elem.children[0].textContent}</li>`;
+        }
+        console.log(clearLi);
+    })
 
