891e77ee6250b416cfa47b9e0af9b7d60addfa5d
Viktor <88923141+Viktor-Streltsov@users.noreply.github.com>
2023-07-02 17:13:41 +0600

    feat: add function

--- main.js
+++ main.js
@@ -12,20 +12,24 @@ const blockEnding = document.querySelector('.ending-text');
 let arr = [];
 let clearLi = [];
 let curentLi = [];
-let textVerifik = '';
-// Созздание тега li по клику на кнопку
+let textVerifik = [];
 
+// Созздание тега li по клику на кнопку
 btn.addEventListener('click', function addtext() {
     let li = document.createElement('li');
     if (input.value !== '') {
-        console.log(textVerifik, input.value)
+        console.log("text: " + textVerifik, input.value)
         li.innerHTML = `<span class="text-li">${input.value}</span> <button class="edit">Editing</button> <button class="clear">Clear</button>`;
-        if(input.value !== textVerifik) {
+
+        // Проверка на идентичность текста
+        let status = textVerifik.indexOf(input.value);
+        if (status === -1) {
             blockAll.append(li);
-            textVerifik = input.value;
+            textVerifik.push(input.value);
         };
         input.value = '';
     };
+
 
     // Очищение фильтров при каждом добовлении
     blockCurent.innerHTML = '';
@@ -37,8 +41,8 @@ btn.addEventListener('click', function addtext() {
     })
 
     // Создание инпута для редактирования тега li
-
     let inp = document.createElement('input');
+    inp.classList.add('input-riade-text');
     li.children[1].addEventListener('click', function () {
         inp.value = li.children[0].textContent;
         li.children[0].textContent = '';
@@ -48,26 +52,38 @@ btn.addEventListener('click', function addtext() {
 
 
     // Добавление нового измененого li через созданные инпут
-
     inp.addEventListener('blur', function () {
         li.children[0].textContent = this.value;
         li.children[1].disabled = false;
     })
 
     // Навешивание класа зачеркивания на выполненый li
+
     li.children[2].addEventListener('click', function () {
         li.children[0].classList.toggle('active');
     })
 
     this.removeEventListener('click', addtext);
-
     return arr.push(li);
 });
 
+// кнопка вывода всех задач
+all.addEventListener('click', function all() {
+    blockAll.classList.add('look-active');
+    blockAll.classList.remove('close-active');
+
+    blockCurent.classList.add('close-active');
+    blockCurent.classList.remove('look-active');
+
+    blockEnding.classList.add('close-active');
+    blockEnding.classList.remove('look-active');
+
+    blockCurent.innerHTML = '';
+    blockEnding.innerHTML = '';
+})
 
 // кнопка вывода выполненых задач
-ending.addEventListener('click', function complit(event) {
-    event.preventDefault();
+ending.addEventListener('click', function complit() {
     // Фильтрация масива li с выполненой задачей
     clearLi = arr.filter(function (elem) {
         if (elem.children[0].classList.contains('active')) {
@@ -75,10 +91,21 @@ ending.addEventListener('click', function complit(event) {
         }
     });
     for (let elemClear of clearLi) {
-        blockEnding.innerHTML += `<li>${elemClear.children[0].textContent}</li>`;
+        blockEnding.innerHTML += `<li class="text-li">${elemClear.children[0].textContent}</li>`;
     }
 
     console.log(clearLi + ' вополненые');
+
+    blockAll.classList.add('close-active');
+    blockAll.classList.remove('look-active');
+
+    blockCurent.classList.add('close-active');
+    blockCurent.classList.remove('look-active')
+
+    blockEnding.classList.add('look-active');
+    blockEnding.classList.remove('close-active');
+
+    blockCurent.innerHTML = '';
 
     // this.removeEventListener('click', complit);
 });
@@ -92,12 +119,21 @@ curent.addEventListener('click', function unful() {
         }
     });
     for (let elemCuret of curentLi) {
-        blockCurent.innerHTML += `<li>${elemCuret.children[0].textContent}</li>`;
+        blockCurent.innerHTML += `<li class="text-li">${elemCuret.children[0].textContent}</li>`;
     }
 
     console.log(curentLi + 'не выполненые');
 
+    blockAll.classList.add('close-active');
+    blockAll.classList.remove('look-active');
 
+    blockCurent.classList.add('look-active');
+    blockCurent.classList.remove('close-active');
+
+    blockEnding.classList.add('close-active');
+    blockEnding.classList.remove('look-active');
+
+    blockEnding.innerHTML = '';
 });
 
 
